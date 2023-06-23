93918c57361b1159793b8266b6621105f8c302e9
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-23 19:47:41 +0200

    Add files via upload

---
+++ Zadaca8-Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Zadaca8_Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> numberQ = new Queue<int>();
+
+            string userInput;
+            do
+            {
+                Console.WriteLine("Enter a number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine("You entered: " + number);
+
+                numberQ.Enqueue(number);
+
+                Console.WriteLine("Do you want enter another number? (y/n)");
+                userInput = Console.ReadLine();
+            } while (userInput == "y");
+
+            Console.WriteLine("Your input numbers are: ");
+
+            while (numberQ.Count > 0)
+            {
+                int number1 = numberQ.Dequeue();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(number1);                
+                Console.ResetColor();
+            }
+        }
+    }
+}
