bd270bdf288b9eb5251fec9e8671279e937bddb0
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-23 19:49:26 +0200

    Add files via upload

---
+++ Zadaca8-Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace Zadaca8_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new() { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The numbers are: ");
+            Console.ResetColor();
+
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            List<int> numbersSquares = numbers.Select(n => n * n).ToList();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The squares of numbers are: ");
+            Console.ResetColor();
+
+            foreach (int square in numbersSquares)
+            {
+                Console.WriteLine(square);
+            }
+        }
+    }
+}
